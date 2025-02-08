e31ea4eb5518becbc9d2426e2ccf34eb9547aa3a
RuoanW <ruoanw2@illinois.edu>
2025-02-08 17:37:41 -0600

    added comments

--- src/lab1.py
+++ src/lab1.py
@@ -7,22 +7,28 @@ min_dist = 20
 
 def main():
     while True:
-        # dist = fc.us.get_distance()
+        # scan with ultrasonic sensor
         scan_list = fc.scan_step(35)
+
+        # if scan_list returns None, continue
         if not scan_list:
             continue
-        # if dist > 0 and dist < min_dist:
+
         if scan_list[3:7] != [2, 2, 2, 2]:
-            fc.stop()
+            # handle obstacle
+            fc.stop() # stop the car
+            fc.backward(speed) # back up for a bit
+            time.sleep(0.3)
+            # choose a random direction
             direction = random.choice([1, 2]) # 1 = left, 2 = right
             turn_time = random.uniform(0.6, 1.5)
-            fc.backward(speed)
-            time.sleep(0.3)
+            # turn to that direction
             if direction == 1:
                 fc.turn_left(speed)
             else:
                 fc.turn_right(speed)
             time.sleep(turn_time)
+            # finally stop, and the car should continue moving forward if there is no obstacle in front
             fc.stop()
         else:
             fc.forward(speed)
