481a1f773b9b4038d3113941f433f07213cd2b4e
RuoanW <ruoanw2@illinois.edu>
2025-02-08 16:46:12 -0600

    lab1.py need testing

--- src/lab1.py
+++ src/lab1.py
@@ -1,15 +1,33 @@
 import picar_4wd as fc
+import time
+import random
+
+speed = 30
+min_dist = 10
 
 def main():
     while True:
-        if fc.get_distance_at(0) > 8:
-            fc.forward(30)
-        else:
+        dist = fc.us.get_distance()
+        if dist > 0 and dist < min_dist:
             fc.stop()
+            direction = random.choice([1, 2]) # 1 = left, 2 = right
+            turn_time = random.uniform(0.6, 1.2)
+            if direction == 1:
+                fc.turn_left(speed)
+            else:
+                fc.turn_right(speed)
+            time.sleep(turn_time)
+            fc.stop()
+        else:
+            fc.forward(speed)
+        time.sleep(0.1)
+
 
 
 if __name__ == '__main__':
     try:
         main()
+    except KeyboardInterrupt:
+        print("Stopped by ^C")
     finally:
         fc.stop()
