620b84e44d5bd8e5534e53160cd67b60e3ce39d3
Patrick Wu <patrickwu8894@gmail.com>
2025-02-15 17:22:58 -0600

    add test for forward speed

--- src/test.py
+++ src/test.py
@@ -2,11 +2,17 @@ import picar_4wd as fc
 import time
 
 def main():
+    # return the distance at 0 degree in cm
     # dist = fc.get_distance_at(0)
     # print(dist)
 
-    fc.turn_left(20)
+    #turn left 90 degree
+    # fc.turn_left(20)
+    # time.sleep(1.1) 
 
+
+    # go forward
+    fc.forward(5)
     time.sleep(1)
 
     fc.stop()
