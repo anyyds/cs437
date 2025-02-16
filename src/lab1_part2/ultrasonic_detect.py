6dba634e848418e139d25f294d2a5c6bb0f398f4
RuoanW <ruoanw2@illinois.edu>
2025-02-16 16:49:20 -0600

    modified navigate.py

--- src/lab1_part2/ultrasonic_detect.py
+++ src/lab1_part2/ultrasonic_detect.py
@@ -2,33 +2,42 @@ import picar_4wd as fc
 import numpy as np
 import math
 
-size = 100
-
-obstacle_grid = np.zeros((size, size))
-
-def mark_obstacle(grid, x, y, clearance = 1):
+def mark_obstacle(grid, grid_size, x, y, clearance = 1):
     for i in range(x - clearance, x + clearance + 1):
         for j in range(y - clearance, y + clearance + 1):
-            if 0 <= i < size and 0 <= j < size:
+            if 0 <= i < grid_size and 0 <= j < grid_size:
                 grid[y, x] = 1
 
-def scan_surroundings():
+def scan_surroundings(cur_pos = (19,39), cur_orientation = 'N', grid_size = 40):
+    cur_x, cur_y = cur_pos
+    # get angle offset from orientation
+    if cur_orientation == 'E':
+        angle_offset = -90
+    elif cur_orientation == 'S':
+        angle_offset = 180
+    elif cur_orientation == 'W':
+        angle_offset = 90
+    else:
+        angle_offset = 0
+    obstacle_grid = np.zeros((grid_size, grid_size))
     for angle_deg in range(-60, 60, 5):
         # set servo angle
         fc.servo.set_angle(angle_deg)
         fc.time.sleep(0.05)
+        abs_angle_deg = angle_offset + angle_deg
         # get distance in cm
         dist = fc.us.get_distance()
         if dist < 0:
             continue
         
-        x = int(round(dist * math.cos(math.radians(angle_deg))))
-        y = int(round(dist * math.sin(math.radians(angle_deg))))
+        x = int(round(dist/10 * math.cos(math.radians(angle_deg)))) + cur_x
+        y = int(round(dist/10 * math.sin(math.radians(angle_deg)))) + cur_y
 
-        mark_obstacle(obstacle_grid, x, y + (size / 2 - 1))
+        mark_obstacle(obstacle_grid, grid_size, x, y + (grid_size / 2 - 1))
+    return obstacle_grid
 
 def main():
-    scan_surroundings()
+    obstacle_grid = scan_surroundings()
     print(obstacle_grid)
 
 if __name__ == "__main__":
