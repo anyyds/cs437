1d71d96be9c9153c58f03ea99530f67e023ce36e
Patrick Wu <patrickwu8894@gmail.com>
2025-02-16 17:20:40 -0600

    add detect_stop_sign

---
+++ src/lab1_part2/detect_stop_sign.py
@@ -0,0 +1,69 @@
+# Code retrieved from https://github.com/tensorflow/examples
+
+# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Main script to run the object detection routine."""
+import time
+
+import cv2
+from picamera2 import Picamera2
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import utils
+import mediapipe as mp
+
+def run() -> bool:
+    model = 'efficientdet_lite0.tflite'
+    width = 1280
+    height = 960
+
+    # Initialize the camera
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_preview_configuration(main={"format": "RGB888", "size": (width, height)}))
+    picam2.start()
+
+    # Initialize the object detection model
+    base_options = python.BaseOptions(model_asset_path=model)
+    options = vision.ObjectDetectorOptions(base_options=base_options,
+                                        score_threshold=0.3,
+                                        max_results=3)
+    detector = vision.ObjectDetector.create_from_options(options)
+
+    # Continuously capture images from the camera and run inference
+    image = picam2.capture_array()
+    image = cv2.rotate(image, cv2.ROTATE_180)  # Change the rotation as needed
+
+    # Convert the image from BGR to RGB as required by the TFLite model.
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Create a TensorImage object from the RGB image.
+    input_tensor = mp.Image(
+        image_format=mp.ImageFormat.SRGB,
+        data=rgb_image
+    )
+
+    # Run object detection estimation using the model.
+    detection_result = detector.detect(input_tensor)
+
+    # Chech if it detect stop sign
+    for detection in detection_result.detections:
+        if detection.label == "stop sign":
+            print("Stop sign detected")
+            return True
+    
+    return False
+
+if __name__ == "__main__":
+    run()
+
