3010ce831fbb3897edf9ae70aca7ec9e81bf5039
Patrick Wu <patrickwu8894@gmail.com>
2025-02-16 17:27:29 -0600

    change StopSignDetector into a class

--- src/lab1_part2/detect_stop_sign.py
+++ src/lab1_part2/detect_stop_sign.py
@@ -23,48 +23,54 @@ from mediapipe.tasks.python import vision
 import utils
 import mediapipe as mp
 
-def run() -> bool:
-    model = 'efficientdet_lite0.tflite'
-    width = 1280
-    height = 960
+class StopSignDetector():
 
-    # Initialize the camera
-    picam2 = Picamera2()
-    picam2.configure(picam2.create_preview_configuration(main={"format": "RGB888", "size": (width, height)}))
-    picam2.start()
+    def __init__(self):
+        model = 'efficientdet_lite0.tflite'
+        width = 1280
+        height = 960
 
-    # Initialize the object detection model
-    base_options = python.BaseOptions(model_asset_path=model)
-    options = vision.ObjectDetectorOptions(base_options=base_options,
-                                        score_threshold=0.3,
-                                        max_results=3)
-    detector = vision.ObjectDetector.create_from_options(options)
+        # Initialize the camera
+        self.picam2 = Picamera2()
+        self.picam2.configure(self.picam2.create_preview_configuration(main={"format": "RGB888", "size": (width, height)}))
+        self.picam2.start()
 
-    # Continuously capture images from the camera and run inference
-    image = picam2.capture_array()
-    image = cv2.rotate(image, cv2.ROTATE_180)  # Change the rotation as needed
+        # Initialize the object detection model
+        base_options = python.BaseOptions(model_asset_path=model)
+        options = vision.ObjectDetectorOptions(base_options=base_options,
+                                            score_threshold=0.3,
+                                            max_results=3)
+        self.detector = vision.ObjectDetector.create_from_options(options)
 
-    # Convert the image from BGR to RGB as required by the TFLite model.
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    def run(self) -> bool:
+        # Continuously capture images from the camera and run inference
+        image = self.picam2.capture_array()
+        image = cv2.rotate(image, cv2.ROTATE_180)  # Change the rotation as needed
 
-    # Create a TensorImage object from the RGB image.
-    input_tensor = mp.Image(
-        image_format=mp.ImageFormat.SRGB,
-        data=rgb_image
-    )
+        # Convert the image from BGR to RGB as required by the TFLite model.
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # Run object detection estimation using the model.
-    detection_result = detector.detect(input_tensor)
+        # Create a TensorImage object from the RGB image.
+        input_tensor = mp.Image(
+            image_format=mp.ImageFormat.SRGB,
+            data=rgb_image
+        )
 
-    # Chech if it detect stop sign
-    for detection in detection_result.detections:
-        if detection.label == "stop sign":
-            print("Stop sign detected")
-            return True
-    
-    print("No stop sign detected")
-    return False
+        # Run object detection estimation using the model.
+        detection_result = self.detector.detect(input_tensor)
+
+        # Chech if it detect stop sign
+        for detection in detection_result.detections:
+            if detection.label == "stop sign":
+                print("Stop sign detected")
+                return True
+        
+        print("No stop sign detected")
+        return False
 
 if __name__ == "__main__":
-    run()
+    stop_sign_detector = StopSignDetector()
+    while True:
+        stop_sign_detector.run()
+        time.sleep(1)
 
